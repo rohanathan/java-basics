5b8d98d785c261660edbb5a8075d9ef130885721
rohanathan <rohanathan.s@hotmail.com>
2024-06-15 09:15:06 +0530

    Updated student details class with constructor

--- OopConcept/src/StudentDetails.java
+++ OopConcept/src/StudentDetails.java
@@ -5,19 +5,27 @@ class Student{
 	double mark2;
 	double mark3;
 	double totalMarks;
+	double averageMark;
 	
-	void setStudDetails(int rollNum,String studName,double mark1,double mark2,double mark3)
+	Student(int rollNum,String studName,double mark1,double mark2,double mark3)
 	{
 		this.rollNum = rollNum;
 		this.studName = studName;
 		this.mark1 = mark1;
 		this.mark2 = mark2;
 		this.mark3 = mark3;
+		calculateTotal();
+		averageMark();
 	}
 	
 	void calculateTotal() 
 	{
 		totalMarks = mark1 + mark2 + mark3;
+	}
+	
+	void averageMark()
+	{
+		averageMark = totalMarks/3;
 	}
 	
 	void displayStudDetails()
@@ -28,16 +36,18 @@ class Student{
 		System.out.println("Mark 2: "+mark2);
 		System.out.println("Mark 3: "+mark3);
 		System.out.println("Total Marks: "+totalMarks);
+		System.out.println("Average Marks: "+averageMark);
 		System.out.println();
 	}
 }
 public class StudentDetails {
 
 	public static void main(String[] args) {
-		Student s1 = new Student();
-		s1.setStudDetails(14,"Rohan", 98.5,79,88.5);
-		s1.calculateTotal();
+		Student s1 = new Student(14,"Rohan", 99,100,87.5);
 		s1.displayStudDetails();
+		
+		Student s2 = new Student(15, "Aravind", 100,100,67);
+		s2.displayStudDetails();
 	}
 
 }
